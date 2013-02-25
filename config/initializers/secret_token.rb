95104f6ca18fcc6506cdb25e7bcc6e67ce4944b6
Jamie Wright <jamie@brilliantfantastic.com>
2013-02-24 21:07:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nda::Application.config.secret_token = '39ade9eab0e607b3729e4c1229ceb455fa185ea3941f520a5b6420d4ec9cbacd2dda6e3bf84934330e220025b6b85aa03125eed47a2a98b6c1c31e3dbac26f1b'
